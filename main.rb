6b724336e94ead0d31d08b8b9a9075fb2fa882aa
Sebastian Sztankowski <sebastiansztankowski@wp.pl>
2024-08-07 10:06:58 +0200

    Translated user input prompt

--- main.rb
+++ main.rb
@@ -5,11 +5,11 @@ require_relative './lib/solver'
 
 def get_number_of_disks
   loop do
-    puts 'Podaj liczbę dysków (od 1 do 9):'
+    print 'Enter the number of disks (from 1 to 9): '
     input = gets.chomp
     return input.to_i if input.match?(/^\d+$/) && (1..9).include?(input.to_i)
 
-    puts 'Nieprawidłowa liczba. Spróbuj ponownie.'
+    puts 'Invalid input. Please try again.'
   end
 end
 
